5adffc66ba22904fdc75b80df7e92966532b8b80
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2025-05-17 20:39:35 +0100

    Add emitente controller

--- adapters/src/main/kotlin/br/com/asoft/nfereader/adapters/in/rest/controller/EmitenteController.kt
+++ adapters/src/main/kotlin/br/com/asoft/nfereader/adapters/in/rest/controller/EmitenteController.kt
@@ -1,23 +1,22 @@
 package br.com.asoft.nfereader.adapters.`in`.rest.controller
 
+import br.com.asoft.nfereader.adapters.out.persistence.mapper.EmitenteMapper.toDto
 import br.com.asoft.nfereader.application.port.`in`.service.EmitenteServicePort
 import br.com.asoft.nfereader.application.port.`in`.service.SecurityUtilPort
-import br.com.asoft.nfereader.model.Emitente
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import br.com.asoft.nfereader.controller.EmitenteApi
+import br.com.asoft.nfereader.model.EmitenteDTO
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("emitente")
 class EmitenteController(
     private val emitenteService: EmitenteServicePort,
     private val securityUtil: SecurityUtilPort
-) {
-    @GetMapping
-    fun getAll(): List<Emitente> {
-        return emitenteService.getAll(
+) : EmitenteApi {
+    override fun getAllEmitent(): ResponseEntity<List<EmitenteDTO>> {
+        val list = this.emitenteService.getAll(
             securityUtil.getIdentityId()
         )
+        return ResponseEntity.ok(list.map { it.toDto() })
     }
-
 }
--- adapters/src/main/kotlin/br/com/asoft/nfereader/adapters/out/persistence/mapper/EmitenteMapper.kt
+++ adapters/src/main/kotlin/br/com/asoft/nfereader/adapters/out/persistence/mapper/EmitenteMapper.kt
@@ -2,8 +2,28 @@ package br.com.asoft.nfereader.adapters.out.persistence.mapper
 
 import br.com.asoft.nfereader.adapters.out.persistence.model.nfe.EmitenteEntity
 import br.com.asoft.nfereader.model.Emitente
+import br.com.asoft.nfereader.model.EmitenteDTO
 
 object EmitenteMapper {
+
+    fun Emitente.toDto() =
+        EmitenteDTO(
+            id = id,
+            cpfCnpj = cnpj,
+            razaoSocial = razaoSocial,
+            nomeFantasia = nomeFantasia,
+            inscricaoEstadual = ie,
+            crt = crt,
+            uf = uf,
+            municipio = municipio,
+            bairro = bairro,
+            telefone = telefone,
+            logradouro = logradouro,
+            cep = cep,
+            cPais = cPais,
+            xPais = xPais
+        )
+
     fun EmitenteEntity.toModel() =
         Emitente(
             id!!,
